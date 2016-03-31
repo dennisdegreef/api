fb8199b3706526d0ae506c4cc4f1a33056c42acf
Uzair Mehmood <hey@illicitr.us>
2016-03-31 05:55:01 +0500

    Update read.php

--- chats/read.php
+++ chats/read.php
@@ -1,8 +1,11 @@
 <?php
-
+//Make sure we get :memberName or All in our call
 if (!empty($parts[4])) {
+    //if the call is for All
     if (!$memberOnly) {
+        //We need all results and all columns here
         $getMessages = $dbh->prepare("SELECT * FROM chatMessages ORDER BY sentTime DESC LIMIT 50");
+        //Try running query and set variables as required
         if ($getMessages->execute()) {
             $rows   = $getMessages->fetchAll(PDO::FETCH_ASSOC);
             $status = "success";
@@ -11,8 +14,12 @@ if (!empty($parts[4])) {
             $status = "error";
             $data   = "Query error";
         }
-    } else {
+    }
+    //if the call is for memberName
+    else {
+        //We need results for provided memberName and all columns here
         $getMessages = $dbh->prepare("SELECT * FROM chatMessages WHERE sender = ? ORDER BY sentTime DESC LIMIT 50");
+        //Try running query and set variables as required
         if ($getMessages->execute(array($member))) {
             $rows   = $getMessages->fetchAll(PDO::FETCH_ASSOC);
             $status = "success";
