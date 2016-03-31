7f4ca5c9284d4ba8d4f8c6bff0ff04d39a2e1fb5
Uzair Mehmood <hey@illicitr.us>
2016-03-31 05:57:54 +0500

    Update create.php

--- chats/create.php
+++ chats/create.php
@@ -1,11 +1,12 @@
 <?php
-
+//Make sure we have both the username and message
 if (!empty($message) && !empty($username)) {
+    //Sanitzation
     $message  = trim(preg_replace('/\s/', " ", htmlentities($message)));
-
+    //Prepare the query
     $sendMessage = $dbh->prepare("INSERT INTO chatMessages (message, sender)
             VALUES (?, ?)");
-
+    //Try running the Query and set status as required
     if ($sendMessage->execute(array($message, $username))) {
         $status = "success";
         $data   = "Message sent";
