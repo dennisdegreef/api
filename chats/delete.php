00304cd6530129c165fc2ce22dfddaf4919af73d
Uzair Mehmood <hey@illicitr.us>
2016-03-31 05:56:15 +0500

    Update delete.php

--- chats/delete.php
+++ chats/delete.php
@@ -1,10 +1,10 @@
 <?php
-
+//Make sure we have the messageId
 if (!empty($messageId)) {
+    //Set Query
     $id = $_POST['messageId'];
-
     $delMessage = $dbh->prepare("DELETE FROM chatMessages WHERE id=?");
-
+    //Try running the query and set status as required
     if ($delMessage->execute(array($id))) {
         $status = "success";
         $data   = "Message deleted";
